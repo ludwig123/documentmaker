071de9d647e2575b596e99e6490b26a46d2b7f19
ludwig123 <hejingjizhen@gmail.com>
2018-09-23 20:52:21 +0800

    和网页交互调通ajax()方式

--- application/maker/controller/Api.php
+++ application/maker/controller/Api.php
@@ -3,6 +3,7 @@ namespace app\maker\controller;
 
 use app\maker\model\Code;
 use think\config\driver\Json;
+use think\facade\Request;
 
 class Api {
 	public function code(){
@@ -17,4 +18,41 @@ class Api {
 		return json($data);
 		
 	}
+
+	/**通过决定书编号找到案卷所有信息
+	 * @param $String $caseNum
+	 */
+	public function findcase($caseNum) {
+	    ;  
+	}
+	
+	/**创建一个案卷
+	 * @param Array $param
+	 */
+	public function createCase() {
+	    $info = Request::instance()->post();
+	    
+	    $data = array(
+	        'data'=>"2",
+	        'code'=>'0',
+	        'count'=>"2"
+	    );
+	    return json($data) ;
+	}
+	
+	/**更新案卷信息
+	 * @param unknown $caseDetail
+	 */
+	public function updateCase($caseDetail) {
+	    ;
+	}
+	
+	/**删除一个案卷
+	 * @param unknown $param
+	 */
+	public function deleteCase($param) {
+	    ;
+	}
+	
+	
 }
--- public/static/maker/tools.js
+++ public/static/maker/tools.js
@@ -1,7 +1,9 @@
-    layui.use(['jquery', 'layer'], function() {
-        var $ = layui.$ //重点处
-        ,
-        layer = layui.layer;
 
-        //后面就跟你平时使用jQuery一样
-    });
+
+    // layui.use(['jquery', 'layer'], function() {
+    //     var $ = layui.$ //重点处
+    //     ,
+    //     layer = layui.layer;
+
+    //     //后面就跟你平时使用jQuery一样
+    // });
